014827c38d666dd2d6b4409b24efc2d6d50b6218
Nicholas Shindler <shindler25@gmail.com>
2020-03-12 16:39:43 +0100

    inital project structure

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Hyperion RGB-d Vision integration with ROS
+ *
+ */
+
+#include <ros/ros.h>
+
+#include "hyperion/cnn.h"
+#include "hyperion/core.h"
+#include "hyperion/kalman_filter.h"
+
+int main(int argc, char **argv) {
+  /**
+   * main function. init and connect withtthe ROS  system
+   *
+   */
+
+  ros::init(argc, argv, "hyperion")
+}
